de75f8c24dde6035d65ce48c7066af3d92edd78e
johnbinu <binujohn73@gmail.com>
2025-03-21 10:31:57 -0700

    Delete DatabaseProjectNorthwind.SQLDatabase directory

--- DatabaseProjectNorthwind.SQLDatabase/dbo/Views/Alphabetical list of products.sql
+++
@@ -1,8 +0,0 @@
-
-create view "Alphabetical list of products" AS
-SELECT Products.*, Categories.CategoryName
-FROM Categories INNER JOIN Products ON Categories.CategoryID = Products.CategoryID
-WHERE (((Products.Discontinued)=0))
-
-GO
-
